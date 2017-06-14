012a3e31c6683fb70b74b495ef06229c38615bf4
gabcarrara <gabcarrara@hotmail.com>
2017-06-14 19:44:09 -0300

    Adição da ação para verificar se lâmpada deve ser ligada com base na hora.

--- planning.pl
+++ planning.pl
@@ -4,9 +4,9 @@
 
 
 %cria predicado
-hora(h1).
-hora(h2).
-hora(h3).
+hora(12).
+hora(19).
+hora(14).
 
 sala(s1).
 sala(s2).
@@ -32,12 +32,6 @@ professor(p1).
 professor(p2).
 professor(p3).
 
-%clima(quente).
-%clima(frio).
-
-%baixa(potencia).
-%media(potencia).
-%alta(potencia).
 potencia(baixa).
 potencia(media).
 potencia(alta).
@@ -60,13 +54,9 @@ pertence(s1, l1).
 pertence(s2, l2).
 pertence(s3, l3).
 
-potencia(a1, t1).
-potencia(a2, t1).
-potencia(a3, t1).
-
-reserva(s1, p1, h1).
-reserva(s1, p2, h2).
-reserva(s2, p3, h1).
+reserva(s1, p1, 19).
+reserva(s1, p2, 14).
+reserva(s2, p3, 12).
 
 usa_equipamento(p3).
 
@@ -78,17 +68,48 @@ nao_usa_equipamento(p2).
 %Logica
 equipamento(X) :- data_show(X); computador(X).
 ligavel(X) :- data_show(X); computador(X); ar_condicionado(X); lampada(X).
+dia(X) :- hora(X), X < 18.
+noite(X) :- hora(X), X >= 18.
 
 
 %Ligar
 can(ligar(Item, Sala), [desligado(Item), aberta(Sala)], preparar) :- 
-ligavel(Item), sala(Sala), pertence(Sala, Item) .
+ligavel(Item), sala(Sala), pertence(Sala, Item).
  
 adds(ligar(Item, Sala), [ligado(Item)], _, preparar) :- 
 ligavel(Item), sala(Sala).
  
 deletes(ligar(Item, Sala), [desligado(Item)], preparar) :- 
 ligavel(Item), sala(Sala).
+ 
+%Verificação da Lâmpada
+
+can(verifica_lampada_dia(Lampada,Sala,Hora), [aberta(Sala)], preparar) :-
+lampada(Lampada), 
+hora(Hora), 
+dia(Hora),
+sala(Sala),
+reserva(Sala, _,Hora).
+
+adds(verifica_lampada_dia(Lampada, Hora, _Sala), [desligado(Lampada) , verificado(Lampada)], _, preparar) :-  
+lampada(Lampada), hora(Hora).
+
+deletes(verifica_lampada_dia(Lampada, Hora, _Sala), [ligado(Lampada)], preparar):-
+lampada(Lampada), hora(Hora).
+
+can(verifica_lampada_noite(Lampada, Hora, Sala), [aberta(Sala)], preparar) :-
+lampada(Lampada), 
+hora(Hora), 
+noite(Hora),
+sala(Sala),
+reserva(Sala,_,Hora).
+
+adds(verifica_lampada_noite(Lampada, Hora, _Sala), [ligado(Lampada), verificado(Lampada)], _, preparar) :-  
+lampada(Lampada), hora(Hora).
+
+deletes(verifica_lampada_noite(Lampada, Hora, _Sala), [desligado(Lampada)], preparar):-
+lampada(Lampada), hora(Hora).
+ 
  
 %Desligar
 can(desligar(Item, Sala), [ligado(Item), aberta(Sala)], preparar) :- 
@@ -142,7 +163,8 @@ deletes(fechar(Sala, Professor, Hora),[aberta(Sala)], preparar) :-
 sala(Sala), professor(Professor), hora(Hora).
  
 %Preparar aula
-can(preparar_aula_com_equipamento(Sala, Professor), [aberta(Sala), ligado(X), ligado(Y), ligado(Z), ligado(W),  ajustado(W)],preparar) :-
+
+can(preparar_aula_com_equipamento(Sala, Professor), [aberta(Sala), ligado(X), ligado(Y), verificado(Z), ligado(W),  ajustado(W)],preparar) :-
 professor(Professor),
 sala(Sala),
 data_show(X),
@@ -167,7 +189,7 @@ professor(Professor),
 sala(Sala).
 
  
-can(preparar_aula_sem_equipamento(Sala, Professor), [aberta(Sala),ligado(Z), ligado(W), ajustado(W)],preparar) :-
+can(preparar_aula_sem_equipamento(Sala, Professor), [aberta(Sala), verificado(Z), ligado(W), ajustado(W)],preparar) :-
 professor(Professor),
 sala(Sala),
 lampada(Z),
@@ -191,4 +213,5 @@ sala(Sala).
 %casos de teste
 
 test01(P) :- plan([ fechada(s1), desligado(a1), desligado(l1), desligado(c1), desligado(d1), nao_ajustado(a1), frio(clima)], [aula_preparada(s1,p1)], preparar, P).
-test02(P) :- plan([ fechada(s1), desligado(a1), desligado(l1), desligado(c1), desligado(d1), nao_ajustado(a1), quente(clima)], [aula_preparada(s1,p1)], preparar, P).
+test02(P) :- plan([ fechada(s1), desligado(a1), desligado(l1), desligado(c1), desligado(d1), nao_ajustado(a1), quente(clima)], [aula_preparada(s1,p2)], preparar, P).
+test03(P) :- plan([ fechada(s2), desligado(a2), desligado(l2), desligado(c2), desligado(d2), nao_ajustado(a2), quente(clima)], [aula_preparada(s2,p3)], preparar, P).
