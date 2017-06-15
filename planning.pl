cf8c0b1470417c6fee149f9f5d602a51a4209a27
gabcarrara <gabcarrara@hotmail.com>
2017-06-15 17:24:18 -0300

    Criação da ação para despreparar a aula e adição dos repectivos casos de teste.

--- planning.pl
+++ planning.pl
@@ -1,7 +1,7 @@
 :- discontiguous can/3.
 :- discontiguous adds/4.
 :- discontiguous deletes/3.
-
+%:- debug.
 
 %Inicio de criação dos predicado
 sala(sala_120).
@@ -113,39 +113,57 @@ deletes(desligar(Item, Sala), [ligado(Item)],preparar) :-
 /**
 *	Caso seja dia, não acende a lâmpada.
 */
-can(acender_lampada_dia(Item, Sala, Hora), [desligado(Item), aberta(Sala), vai_dar_aula(Sala, Professor, Hora)], preparar) :- 
+can(acender_luz_dia(Item, Sala, Hora), [desligado(Item), aberta(Sala), vai_dar_aula(Sala, Professor, Hora)], preparar) :- 
 	lampada(Item), 
 	sala(Sala),
 	reserva(Sala, Professor, Hora), 
 	pertence(Sala, Item), 
 	dia(Hora).
  
-adds(acender_lampada_dia(Item, Sala, _Hora), [verificado(Item), desligado(Item)], _, preparar) :- 
+adds(acender_luz_dia(Item, Sala, _Hora), [verificado(Item), desligado(Item)], _, preparar) :- 
 	lampada(Item), 
 	sala(Sala).
  
-deletes(acender_lampada_dia(Item, Sala, _Hora), [ligado(Item)], preparar) :- 
+deletes(acender_luz_dia(Item, Sala, _Hora), [ligado(Item)], preparar) :- 
 	lampada(Item), 
 	sala(Sala).
 
 /**
 *	Caso seja noite, acende a lâmpada.
 */
-can(acender_lampada_noite(Item, Sala, Hora), [desligado(Item), aberta(Sala), vai_dar_aula(Sala, Professor, Hora)], preparar) :- 
+can(acender_luz_noite(Item, Sala, Hora), [desligado(Item), aberta(Sala), vai_dar_aula(Sala, Professor, Hora)], preparar) :- 
 	lampada(Item), 
 	sala(Sala),
 	reserva(Sala, Professor, Hora), 
 	pertence(Sala, Item), 
 	noite(Hora).
  
-adds(acender_lampada_noite(Item, Sala, _Hora), [verificado(Item) ,ligado(Item)], _, preparar) :- 
+adds(acender_luz_noite(Item, Sala, _Hora), [verificado(Item) ,ligado(Item)], _, preparar) :- 
 	lampada(Item), 
 	sala(Sala).
  
-deletes(acender_lampada_noite(Item, Sala, _Hora), [desligado(Item)], preparar) :- 
+deletes(acender_luz_noite(Item, Sala, _Hora), [desligado(Item)], preparar) :- 
 	lampada(Item), 
 	sala(Sala).
-  
+	
+/**
+* Apaga a luz caso ela estaja acesa 
+*/	
+can(apagar_luz(Item, Sala), [ligado(Item), deu_aula(Sala, Professor, Hora), aberta(Sala)], preparar):-
+	lampada(Item),
+	sala(Sala),
+	reserva(Sala, Professor, Hora),
+	pertence(Sala, Item).
+	
+adds(apagar_luz(Item, Sala), [desligado(Item)], _,preparar):-
+	lampada(Item),
+	sala(Sala),
+	pertence(Sala, Item).
+	
+deletes(apagar_luz(Item, Sala), [ligado(Item)], preparar):-
+	lampada(Item),
+	sala(Sala),
+	pertence(Sala, Item).
   
 /**
 *	Caso esteja frio, ajusta ar-condicionado para o frio. 
@@ -198,12 +216,20 @@ deletes(abrir(Sala, Professor, _Hora),[fechada(Sala)], preparar) :-
 /**
 *	Caso a sala tenha sido reservado para o professor no horario X, fechar a sala.
 */
-can(fechar(Sala, Professor, Hora), [aberta(Sala),vai_dar_aula(Sala, Professor, Hora)], preparar) :-
+can(fechar(Sala, Professor, Hora), [aberta(Sala), deu_aula(Sala, Professor, Hora), desligado(X), desligado(Y), desligado(Z), desligado(W)], preparar) :-
 	sala(Sala), 
+	datashow(X),
+	pertence(Sala, X),
+	computador(Y),
+	pertence(Sala, Y),
+	lampada(Z),
+	pertence(Sala,Z),
+	ar_condicionado(W),
+	pertence(Sala, W),
 	professor(Professor), 
 	reserva(Sala, Professor, Hora).
  
-adds(fechar(Sala, Professor, _Hora), [ fechada(Sala)], _, preparar) :- 
+adds(fechar(Sala, Professor, _Hora), [fechada(Sala)], _, preparar) :- 
 	sala(Sala), 
 	professor(Professor).
  
@@ -262,6 +288,32 @@ deletes(preparar_aula_sem_equipamento(Sala, Professor, Hora), [], preparar):-
 	sala(Sala),
 	reserva(Sala, Professor, Hora).
 
+/**	
+* Desprepera a aula após desligar os itens da sala e fechar a sala	
+*/	
+can(despreparar_aula(Sala, Professor, Hora),[aula_preparada(Sala, Professor) ,fechada(Sala), deu_aula(Sala, Professor, Hora), desligado(X), desligado(Y), desligado(Z), desligado(W) ], preparar):-
+	professor(Professor),
+	sala(Sala),
+	datashow(X),
+	pertence(Sala, X),
+	computador(Y),
+	pertence(Sala, Y),
+	lampada(Z),
+	pertence(Sala,Z),
+	ar_condicionado(W),
+	pertence(Sala, W),
+	reserva(Sala, Professor, Hora).
+
+adds(despreparar_aula(Sala, Professor, Hora), [aula_despreparada(Sala, Professor)], _, preparar):-
+	professor(Professor),
+	sala(Sala),
+	reserva(Sala, Professor, Hora).
+	
+deletes(despreparar_aula(Sala, Professor, Hora), [aula_preparada(Sala, Professor)], preparar):-
+	professor(Professor),
+	sala(Sala),
+	reserva(Sala, Professor, Hora).
+	
 
 /**
 *	Casos de testes
@@ -271,3 +323,7 @@ test01(P) :- plan([vai_dar_aula(sala_120,aline,19), fechada(sala_120), desligado
 test02(P) :- plan([vai_dar_aula(sala_120,dante,14), fechada(sala_120), desligado(ar_condicionado_01), desligado(lampada_01), desligado(compact), desligado(datashow_01), nao_ajustado(ar_condicionado_01), quente(clima)], [aula_preparada(sala_120,dante)], preparar, P).
 
 test03(P) :- plan([vai_dar_aula(sala_230,murta,12) ,fechada(sala_230), desligado(ar_condicionado_02), desligado(lampada_02), desligado(dell), desligado(datashow_02), nao_ajustado(ar_condicionado_02), quente(clima)], [aula_preparada(sala_230,murta)], preparar, P).
+
+test04(P) :- plan([deu_aula(sala_230,murta,12), aula_preparada(sala_230, murta) ,aberta(sala_230), ligado(ar_condicionado_02), desligado(lampada_02), ligado(dell), ligado(datashow_02)], [aula_despreparada(sala_230,murta)], preparar, P).
+
+test05(P) :- plan([deu_aula(sala_120,aline,19), aula_preparada(sala_120, aline) ,aberta(sala_120), ligado(ar_condicionado_01), ligado(lampada_01), desligado(compact), desligado(datashow_01)], [aula_despreparada(sala_120,aline)], preparar, P).
